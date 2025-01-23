99886718dd063dca39c9234d820a638c55010061
silentsilas <me@silentsilas.com>
2025-01-23 18:59:39 -0500

    update node, try out threlte uikit

--- src/lib/components/scenes/editor/Scene.svelte
+++ src/lib/components/scenes/editor/Scene.svelte
@@ -1,12 +1,17 @@
 <script lang="ts" module>
 	import { T } from '@threlte/core';
+	import { Root, Container, Fullscreen, Text } from 'threlte-uikit';
 </script>
 
 <T.PerspectiveCamera position={[0, 0, 5]} />
 
 <T.Group>
-	<T.Mesh position={[ 0, -0.2, 0 ]}>
-		<T.BoxGeometry args={[1, 1, 1]} />
-		<T.MeshBasicMaterial color="red" />
+	<Root backgroundColor="red" sizeX={8} sizeY={4} flexDirection="row">
+		<Container flexGrow={1} margin={32} backgroundColor="green" />
+		<Container flexGrow={1} margin={32} backgroundColor="blue" />
+	</Root>
+	<T.Mesh position={[0, -0.2, 0]} visible>
+		<T.BoxGeometry args={[3, 5, 0.1]} />
+		<T.MeshStandardMaterial emissive="#187d18" />
 	</T.Mesh>
 </T.Group>
---
+++ src/lib/components/scenes/game/Title.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Root, Container } from 'threlte-uikit';
+</script>
+
+<Root backgroundColor="red" sizeX={8} sizeY={4} flexDirection="row">
+	<Container flexGrow={1} margin={32} backgroundColor="green" />
+	<Container flexGrow={1} margin={32} backgroundColor="blue" />
+</Root>
